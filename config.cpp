23e626cc7041ca4ee4e4ff59484d58cf56e11383
Jeff Day <jeffday@gmail.com>
2023-10-31 23:21:45 -0500

    hell yeah, figured it out. enable TWM 308 rifles to take BO mags

--- config.cpp
+++ config.cpp
@@ -15,11 +15,20 @@ class CfgPatches
 			"DZ_Weapons_Firearms_M4",
 			"DZ_Weapons_Firearms_M16A2",
 			"DZ_Weapons_Firearms_FAL", 
-			"DZ_Weapons_Firearms_AKM", 
-			"TWM_Guns"
-			// "ASC_Weapons_LVOAC",
-			// "ASC_Weapons_HK416",
-			// "Blackout_Weapons_Snipers"
+			"DZ_Weapons_Firearms_AKM",
+			"DZ_Weapons_Firearms_Ruger1022",
+			"AK105",
+			"G36K",
+			"HK416",
+			"IA2",
+			"IA2H",
+			"MK18", 
+			"SR47",
+			"TWM_M14",
+			"TWM_M1A",
+			"BO_L96",
+			"ASC_Weapons_LVOAC",
+			"ASC_Weapons_HK416"
 		};
 		ammo[] = {};
 	};
@@ -264,20 +273,20 @@ class cfgWeapons
 			"BO_Mag_GEN2_100Rnd"
 		};
 	};
-	// class AKM_Base;
-	// class TWM_AK105_Base: AKM_Base
-	// {
-	// 	magazines[]=
-	// 	{
-	// 		"BO_AK74_30rnd",
-	// 		"TWM_Mag_AK105",
-	// 		"TWM_Mag_AK105_60rnd",
-	// 		"Mag_AK74_30Rnd",
-	// 		"Mag_AK74_30Rnd_Black",
-	// 		"Mag_AK74_30Rnd_Green",
-	// 		"Mag_AK74_45Rnd"
-	// 	};
-	// };
+	class AKM_Base;
+	class TWM_AK105_Base: AKM_Base
+	{
+		magazines[]=
+		{
+			"BO_AK74_30rnd",
+			"TWM_Mag_AK105",
+			"TWM_Mag_AK105_60rnd",
+			"Mag_AK74_30Rnd",
+			"Mag_AK74_30Rnd_Black",
+			"Mag_AK74_30Rnd_Green",
+			"Mag_AK74_45Rnd"
+		};
+	};
 	class TWM_HK416_Base: M4A1_Base
 	{
 		magazines[]=
@@ -325,196 +334,196 @@ class cfgWeapons
 			"BO_Mag_GEN2_100Rnd"
 		};
 	};
-	// class FAL_Base;
-	// class TWM_G36K_Base: FAL_Base
-	// {
-	// 	magazines[]=
-	// 	{
-	// 		"TWM_Mag_G36K",
-	// 		"Mag_STANAG_30Rnd",
-	// 		"Mag_STANAGCoupled_30Rnd",
-	// 		"Mag_STANAG_60Rnd",
-	// 		"Mag_CMAG_10Rnd",
-	// 		"Mag_CMAG_20Rnd",
-	// 		"Mag_CMAG_30Rnd",
-	// 		"Mag_CMAG_40Rnd",
-	// 		"Mag_CMAG_10Rnd_Green",
-	// 		"Mag_CMAG_20Rnd_Green",
-	// 		"Mag_CMAG_30Rnd_Green",
-	// 		"Mag_CMAG_40Rnd_Green",
-	// 		"Mag_CMAG_10Rnd_Black",
-	// 		"Mag_CMAG_20Rnd_Black",
-	// 		"Mag_CMAG_30Rnd_Black",
-	// 		"Mag_CMAG_40Rnd_Black",
-	// 		"Mag_ASC_Stanag_10Rnd",
-	// 		"Mag_ASC_Stanag_10Rnd_Camo",
-	// 		"Mag_ASC_Stanag_10Rnd_DigiTan",
-	// 		"Mag_ASC_Stanag_10Rnd_Tan",
-	// 		"Mag_ASC_Stanag_10Rnd_UCP",
-	// 		"Mag_ASC_Stanag_20Rnd",
-	// 		"Mag_ASC_Stanag_20Rnd_Camo",
-	// 		"Mag_ASC_Stanag_20Rnd_DigiTan",
-	// 		"Mag_ASC_Stanag_20Rnd_Tan",
-	// 		"Mag_ASC_Stanag_20Rnd_UCP",
-	// 		"Mag_ASC_Stanag_100Rnd",
-	// 		"Mag_ASC_Stanag_100Rnd_Camo",
-	// 		"Mag_ASC_Stanag_100Rnd_DigiTan",
-	// 		"Mag_ASC_Stanag_100Rnd_Tan",
-	// 		"Mag_ASC_Stanag_100Rnd_UCP",
-	// 		"Mag_ASC_PMag_60Rnd",
-	// 		"Mag_ASC_PMag_60Rnd_Camo",
-	// 		"Mag_ASC_PMag_60Rnd_DigiTan",
-	// 		"Mag_ASC_PMag_60Rnd_Tan",
-	// 		"Mag_ASC_PMag_60Rnd_UCP",
-	// 		"BO_Mag_PMAG_30Rnd",
-	// 		"BO_Mag_PMAG_Tan_30Rnd",
-	// 		"BO_Mag_PMAG_Green_30Rnd",
-	// 		"BO_Mag_PMAGWindow_30Rnd", 
-	// 		"BO_Mag_D60_60Rnd",
-	// 		"BO_Mag_GEN2_100Rnd"
-	// 	};
-	// };
-	// class TWM_Howa20_Base: FAL_Base
-	// {
-	// 	magazines[]=
-	// 	{
-	// 		"Mag_STANAG_30Rnd",
-	// 		"Mag_STANAGCoupled_30Rnd",
-	// 		"Mag_STANAG_60Rnd",
-	// 		"Mag_CMAG_10Rnd",
-	// 		"Mag_CMAG_20Rnd",
-	// 		"Mag_CMAG_30Rnd",
-	// 		"Mag_CMAG_40Rnd",
-	// 		"Mag_CMAG_10Rnd_Green",
-	// 		"Mag_CMAG_20Rnd_Green",
-	// 		"Mag_CMAG_30Rnd_Green",
-	// 		"Mag_CMAG_40Rnd_Green",
-	// 		"Mag_CMAG_10Rnd_Black",
-	// 		"Mag_CMAG_20Rnd_Black",
-	// 		"Mag_CMAG_30Rnd_Black",
-	// 		"Mag_CMAG_40Rnd_Black",
-	// 		"Mag_ASC_Stanag_10Rnd",
-	// 		"Mag_ASC_Stanag_10Rnd_Camo",
-	// 		"Mag_ASC_Stanag_10Rnd_DigiTan",
-	// 		"Mag_ASC_Stanag_10Rnd_Tan",
-	// 		"Mag_ASC_Stanag_10Rnd_UCP",
-	// 		"Mag_ASC_Stanag_20Rnd",
-	// 		"Mag_ASC_Stanag_20Rnd_Camo",
-	// 		"Mag_ASC_Stanag_20Rnd_DigiTan",
-	// 		"Mag_ASC_Stanag_20Rnd_Tan",
-	// 		"Mag_ASC_Stanag_20Rnd_UCP",
-	// 		"Mag_ASC_Stanag_100Rnd",
-	// 		"Mag_ASC_Stanag_100Rnd_Camo",
-	// 		"Mag_ASC_Stanag_100Rnd_DigiTan",
-	// 		"Mag_ASC_Stanag_100Rnd_Tan",
-	// 		"Mag_ASC_Stanag_100Rnd_UCP",
-	// 		"Mag_ASC_PMag_60Rnd",
-	// 		"Mag_ASC_PMag_60Rnd_Camo",
-	// 		"Mag_ASC_PMag_60Rnd_DigiTan",
-	// 		"Mag_ASC_PMag_60Rnd_Tan",
-	// 		"Mag_ASC_PMag_60Rnd_UCP",
-	// 		"BO_Mag_PMAG_30Rnd",
-	// 		"BO_Mag_PMAG_Tan_30Rnd",
-	// 		"BO_Mag_PMAG_Green_30Rnd",
-	// 		"BO_Mag_PMAGWindow_30Rnd", 
-	// 		"BO_Mag_D60_60Rnd",
-	// 		"BO_Mag_GEN2_100Rnd"
-	// 	};
-	// };
-	// class TWM_IA2_Base: FAL_Base
-	// {
-	// 	magazines[]=
-	// 	{
-	// 		"Mag_STANAG_30Rnd",
-	// 		"Mag_STANAGCoupled_30Rnd",
-	// 		"Mag_STANAG_60Rnd",
-	// 		"Mag_CMAG_10Rnd",
-	// 		"Mag_CMAG_20Rnd",
-	// 		"Mag_CMAG_30Rnd",
-	// 		"Mag_CMAG_40Rnd",
-	// 		"Mag_CMAG_10Rnd_Green",
-	// 		"Mag_CMAG_20Rnd_Green",
-	// 		"Mag_CMAG_30Rnd_Green",
-	// 		"Mag_CMAG_40Rnd_Green",
-	// 		"Mag_CMAG_10Rnd_Black",
-	// 		"Mag_CMAG_20Rnd_Black",
-	// 		"Mag_CMAG_30Rnd_Black",
-	// 		"Mag_CMAG_40Rnd_Black",
-	// 		"Mag_ASC_Stanag_10Rnd",
-	// 		"Mag_ASC_Stanag_10Rnd_Camo",
-	// 		"Mag_ASC_Stanag_10Rnd_DigiTan",
-	// 		"Mag_ASC_Stanag_10Rnd_Tan",
-	// 		"Mag_ASC_Stanag_10Rnd_UCP",
-	// 		"Mag_ASC_Stanag_20Rnd",
-	// 		"Mag_ASC_Stanag_20Rnd_Camo",
-	// 		"Mag_ASC_Stanag_20Rnd_DigiTan",
-	// 		"Mag_ASC_Stanag_20Rnd_Tan",
-	// 		"Mag_ASC_Stanag_20Rnd_UCP",
-	// 		"Mag_ASC_Stanag_100Rnd",
-	// 		"Mag_ASC_Stanag_100Rnd_Camo",
-	// 		"Mag_ASC_Stanag_100Rnd_DigiTan",
-	// 		"Mag_ASC_Stanag_100Rnd_Tan",
-	// 		"Mag_ASC_Stanag_100Rnd_UCP",
-	// 		"Mag_ASC_PMag_60Rnd",
-	// 		"Mag_ASC_PMag_60Rnd_Camo",
-	// 		"Mag_ASC_PMag_60Rnd_DigiTan",
-	// 		"Mag_ASC_PMag_60Rnd_Tan",
-	// 		"Mag_ASC_PMag_60Rnd_UCP",
-	// 		"BO_Mag_PMAG_30Rnd",
-	// 		"BO_Mag_PMAG_Tan_30Rnd",
-	// 		"BO_Mag_PMAG_Green_30Rnd",
-	// 		"BO_Mag_PMAGWindow_30Rnd", 
-	// 		"BO_Mag_D60_60Rnd",
-	// 		"BO_Mag_GEN2_100Rnd"
-	// 	};
-	// };
-	// class TWM_MK18_Base: M4A1_Base
-	// {
-	// 	magazines[]=
-	// 	{
-	// 		"Mag_STANAG_30Rnd",
-	// 		"Mag_STANAGCoupled_30Rnd",
-	// 		"Mag_STANAG_60Rnd",
-	// 		"Mag_CMAG_10Rnd",
-	// 		"Mag_CMAG_20Rnd",
-	// 		"Mag_CMAG_30Rnd",
-	// 		"Mag_CMAG_40Rnd",
-	// 		"Mag_CMAG_10Rnd_Green",
-	// 		"Mag_CMAG_20Rnd_Green",
-	// 		"Mag_CMAG_30Rnd_Green",
-	// 		"Mag_CMAG_40Rnd_Green",
-	// 		"Mag_CMAG_10Rnd_Black",
-	// 		"Mag_CMAG_20Rnd_Black",
-	// 		"Mag_CMAG_30Rnd_Black",
-	// 		"Mag_CMAG_40Rnd_Black",
-	// 		"Mag_ASC_Stanag_10Rnd",
-	// 		"Mag_ASC_Stanag_10Rnd_Camo",
-	// 		"Mag_ASC_Stanag_10Rnd_DigiTan",
-	// 		"Mag_ASC_Stanag_10Rnd_Tan",
-	// 		"Mag_ASC_Stanag_10Rnd_UCP",
-	// 		"Mag_ASC_Stanag_20Rnd",
-	// 		"Mag_ASC_Stanag_20Rnd_Camo",
-	// 		"Mag_ASC_Stanag_20Rnd_DigiTan",
-	// 		"Mag_ASC_Stanag_20Rnd_Tan",
-	// 		"Mag_ASC_Stanag_20Rnd_UCP",
-	// 		"Mag_ASC_Stanag_100Rnd",
-	// 		"Mag_ASC_Stanag_100Rnd_Camo",
-	// 		"Mag_ASC_Stanag_100Rnd_DigiTan",
-	// 		"Mag_ASC_Stanag_100Rnd_Tan",
-	// 		"Mag_ASC_Stanag_100Rnd_UCP",
-	// 		"Mag_ASC_PMag_60Rnd",
-	// 		"Mag_ASC_PMag_60Rnd_Camo",
-	// 		"Mag_ASC_PMag_60Rnd_DigiTan",
-	// 		"Mag_ASC_PMag_60Rnd_Tan",
-	// 		"Mag_ASC_PMag_60Rnd_UCP",
-	// 		"BO_Mag_PMAG_30Rnd",
-	// 		"BO_Mag_PMAG_Tan_30Rnd",
-	// 		"BO_Mag_PMAG_Green_30Rnd",
-	// 		"BO_Mag_PMAGWindow_30Rnd", 
-	// 		"BO_Mag_D60_60Rnd",
-	// 		"BO_Mag_GEN2_100Rnd"
-	// 	};
-	// };
+	class FAL_Base;
+	class TWM_G36K_Base: FAL_Base
+	{
+		magazines[]=
+		{
+			"TWM_Mag_G36K",
+			"Mag_STANAG_30Rnd",
+			"Mag_STANAGCoupled_30Rnd",
+			"Mag_STANAG_60Rnd",
+			"Mag_CMAG_10Rnd",
+			"Mag_CMAG_20Rnd",
+			"Mag_CMAG_30Rnd",
+			"Mag_CMAG_40Rnd",
+			"Mag_CMAG_10Rnd_Green",
+			"Mag_CMAG_20Rnd_Green",
+			"Mag_CMAG_30Rnd_Green",
+			"Mag_CMAG_40Rnd_Green",
+			"Mag_CMAG_10Rnd_Black",
+			"Mag_CMAG_20Rnd_Black",
+			"Mag_CMAG_30Rnd_Black",
+			"Mag_CMAG_40Rnd_Black",
+			"Mag_ASC_Stanag_10Rnd",
+			"Mag_ASC_Stanag_10Rnd_Camo",
+			"Mag_ASC_Stanag_10Rnd_DigiTan",
+			"Mag_ASC_Stanag_10Rnd_Tan",
+			"Mag_ASC_Stanag_10Rnd_UCP",
+			"Mag_ASC_Stanag_20Rnd",
+			"Mag_ASC_Stanag_20Rnd_Camo",
+			"Mag_ASC_Stanag_20Rnd_DigiTan",
+			"Mag_ASC_Stanag_20Rnd_Tan",
+			"Mag_ASC_Stanag_20Rnd_UCP",
+			"Mag_ASC_Stanag_100Rnd",
+			"Mag_ASC_Stanag_100Rnd_Camo",
+			"Mag_ASC_Stanag_100Rnd_DigiTan",
+			"Mag_ASC_Stanag_100Rnd_Tan",
+			"Mag_ASC_Stanag_100Rnd_UCP",
+			"Mag_ASC_PMag_60Rnd",
+			"Mag_ASC_PMag_60Rnd_Camo",
+			"Mag_ASC_PMag_60Rnd_DigiTan",
+			"Mag_ASC_PMag_60Rnd_Tan",
+			"Mag_ASC_PMag_60Rnd_UCP",
+			"BO_Mag_PMAG_30Rnd",
+			"BO_Mag_PMAG_Tan_30Rnd",
+			"BO_Mag_PMAG_Green_30Rnd",
+			"BO_Mag_PMAGWindow_30Rnd", 
+			"BO_Mag_D60_60Rnd",
+			"BO_Mag_GEN2_100Rnd"
+		};
+	};
+	class TWM_Howa20_Base: FAL_Base
+	{
+		magazines[]=
+		{
+			"Mag_STANAG_30Rnd",
+			"Mag_STANAGCoupled_30Rnd",
+			"Mag_STANAG_60Rnd",
+			"Mag_CMAG_10Rnd",
+			"Mag_CMAG_20Rnd",
+			"Mag_CMAG_30Rnd",
+			"Mag_CMAG_40Rnd",
+			"Mag_CMAG_10Rnd_Green",
+			"Mag_CMAG_20Rnd_Green",
+			"Mag_CMAG_30Rnd_Green",
+			"Mag_CMAG_40Rnd_Green",
+			"Mag_CMAG_10Rnd_Black",
+			"Mag_CMAG_20Rnd_Black",
+			"Mag_CMAG_30Rnd_Black",
+			"Mag_CMAG_40Rnd_Black",
+			"Mag_ASC_Stanag_10Rnd",
+			"Mag_ASC_Stanag_10Rnd_Camo",
+			"Mag_ASC_Stanag_10Rnd_DigiTan",
+			"Mag_ASC_Stanag_10Rnd_Tan",
+			"Mag_ASC_Stanag_10Rnd_UCP",
+			"Mag_ASC_Stanag_20Rnd",
+			"Mag_ASC_Stanag_20Rnd_Camo",
+			"Mag_ASC_Stanag_20Rnd_DigiTan",
+			"Mag_ASC_Stanag_20Rnd_Tan",
+			"Mag_ASC_Stanag_20Rnd_UCP",
+			"Mag_ASC_Stanag_100Rnd",
+			"Mag_ASC_Stanag_100Rnd_Camo",
+			"Mag_ASC_Stanag_100Rnd_DigiTan",
+			"Mag_ASC_Stanag_100Rnd_Tan",
+			"Mag_ASC_Stanag_100Rnd_UCP",
+			"Mag_ASC_PMag_60Rnd",
+			"Mag_ASC_PMag_60Rnd_Camo",
+			"Mag_ASC_PMag_60Rnd_DigiTan",
+			"Mag_ASC_PMag_60Rnd_Tan",
+			"Mag_ASC_PMag_60Rnd_UCP",
+			"BO_Mag_PMAG_30Rnd",
+			"BO_Mag_PMAG_Tan_30Rnd",
+			"BO_Mag_PMAG_Green_30Rnd",
+			"BO_Mag_PMAGWindow_30Rnd", 
+			"BO_Mag_D60_60Rnd",
+			"BO_Mag_GEN2_100Rnd"
+		};
+	};
+	class TWM_IA2_Base: FAL_Base
+	{
+		magazines[]=
+		{
+			"Mag_STANAG_30Rnd",
+			"Mag_STANAGCoupled_30Rnd",
+			"Mag_STANAG_60Rnd",
+			"Mag_CMAG_10Rnd",
+			"Mag_CMAG_20Rnd",
+			"Mag_CMAG_30Rnd",
+			"Mag_CMAG_40Rnd",
+			"Mag_CMAG_10Rnd_Green",
+			"Mag_CMAG_20Rnd_Green",
+			"Mag_CMAG_30Rnd_Green",
+			"Mag_CMAG_40Rnd_Green",
+			"Mag_CMAG_10Rnd_Black",
+			"Mag_CMAG_20Rnd_Black",
+			"Mag_CMAG_30Rnd_Black",
+			"Mag_CMAG_40Rnd_Black",
+			"Mag_ASC_Stanag_10Rnd",
+			"Mag_ASC_Stanag_10Rnd_Camo",
+			"Mag_ASC_Stanag_10Rnd_DigiTan",
+			"Mag_ASC_Stanag_10Rnd_Tan",
+			"Mag_ASC_Stanag_10Rnd_UCP",
+			"Mag_ASC_Stanag_20Rnd",
+			"Mag_ASC_Stanag_20Rnd_Camo",
+			"Mag_ASC_Stanag_20Rnd_DigiTan",
+			"Mag_ASC_Stanag_20Rnd_Tan",
+			"Mag_ASC_Stanag_20Rnd_UCP",
+			"Mag_ASC_Stanag_100Rnd",
+			"Mag_ASC_Stanag_100Rnd_Camo",
+			"Mag_ASC_Stanag_100Rnd_DigiTan",
+			"Mag_ASC_Stanag_100Rnd_Tan",
+			"Mag_ASC_Stanag_100Rnd_UCP",
+			"Mag_ASC_PMag_60Rnd",
+			"Mag_ASC_PMag_60Rnd_Camo",
+			"Mag_ASC_PMag_60Rnd_DigiTan",
+			"Mag_ASC_PMag_60Rnd_Tan",
+			"Mag_ASC_PMag_60Rnd_UCP",
+			"BO_Mag_PMAG_30Rnd",
+			"BO_Mag_PMAG_Tan_30Rnd",
+			"BO_Mag_PMAG_Green_30Rnd",
+			"BO_Mag_PMAGWindow_30Rnd", 
+			"BO_Mag_D60_60Rnd",
+			"BO_Mag_GEN2_100Rnd"
+		};
+	};
+	class TWM_MK18_Base: M4A1_Base
+	{
+		magazines[]=
+		{
+			"Mag_STANAG_30Rnd",
+			"Mag_STANAGCoupled_30Rnd",
+			"Mag_STANAG_60Rnd",
+			"Mag_CMAG_10Rnd",
+			"Mag_CMAG_20Rnd",
+			"Mag_CMAG_30Rnd",
+			"Mag_CMAG_40Rnd",
+			"Mag_CMAG_10Rnd_Green",
+			"Mag_CMAG_20Rnd_Green",
+			"Mag_CMAG_30Rnd_Green",
+			"Mag_CMAG_40Rnd_Green",
+			"Mag_CMAG_10Rnd_Black",
+			"Mag_CMAG_20Rnd_Black",
+			"Mag_CMAG_30Rnd_Black",
+			"Mag_CMAG_40Rnd_Black",
+			"Mag_ASC_Stanag_10Rnd",
+			"Mag_ASC_Stanag_10Rnd_Camo",
+			"Mag_ASC_Stanag_10Rnd_DigiTan",
+			"Mag_ASC_Stanag_10Rnd_Tan",
+			"Mag_ASC_Stanag_10Rnd_UCP",
+			"Mag_ASC_Stanag_20Rnd",
+			"Mag_ASC_Stanag_20Rnd_Camo",
+			"Mag_ASC_Stanag_20Rnd_DigiTan",
+			"Mag_ASC_Stanag_20Rnd_Tan",
+			"Mag_ASC_Stanag_20Rnd_UCP",
+			"Mag_ASC_Stanag_100Rnd",
+			"Mag_ASC_Stanag_100Rnd_Camo",
+			"Mag_ASC_Stanag_100Rnd_DigiTan",
+			"Mag_ASC_Stanag_100Rnd_Tan",
+			"Mag_ASC_Stanag_100Rnd_UCP",
+			"Mag_ASC_PMag_60Rnd",
+			"Mag_ASC_PMag_60Rnd_Camo",
+			"Mag_ASC_PMag_60Rnd_DigiTan",
+			"Mag_ASC_PMag_60Rnd_Tan",
+			"Mag_ASC_PMag_60Rnd_UCP",
+			"BO_Mag_PMAG_30Rnd",
+			"BO_Mag_PMAG_Tan_30Rnd",
+			"BO_Mag_PMAG_Green_30Rnd",
+			"BO_Mag_PMAGWindow_30Rnd", 
+			"BO_Mag_D60_60Rnd",
+			"BO_Mag_GEN2_100Rnd"
+		};
+	};
 	class TWM_SR47_Base: M4A1_Base
 	{
 		magazines[]=
@@ -530,104 +539,142 @@ class cfgWeapons
 			"BO_PMAG_AKM_30rnd_FDE"
 		};
 	};
-	// class ASC_LVOAC_Base: Rifle_Base
-	// {
-	// 	magazines[]=
-	// 	{
-	// 		"Mag_STANAG_30Rnd",
-	// 		"Mag_STANAGCoupled_30Rnd",
-	// 		"Mag_STANAG_60Rnd",
-	// 		"Mag_CMAG_10Rnd",
-	// 		"Mag_CMAG_20Rnd",
-	// 		"Mag_CMAG_30Rnd",
-	// 		"Mag_CMAG_40Rnd",
-	// 		"Mag_CMAG_10Rnd_Green",
-	// 		"Mag_CMAG_20Rnd_Green",
-	// 		"Mag_CMAG_30Rnd_Green",
-	// 		"Mag_CMAG_40Rnd_Green",
-	// 		"Mag_CMAG_10Rnd_Black",
-	// 		"Mag_CMAG_20Rnd_Black",
-	// 		"Mag_CMAG_30Rnd_Black",
-	// 		"Mag_CMAG_40Rnd_Black",
-	// 		"Mag_ASC_Stanag_10Rnd",
-	// 		"Mag_ASC_Stanag_10Rnd_Camo",
-	// 		"Mag_ASC_Stanag_10Rnd_DigiTan",
-	// 		"Mag_ASC_Stanag_10Rnd_Tan",
-	// 		"Mag_ASC_Stanag_10Rnd_UCP",
-	// 		"Mag_ASC_Stanag_20Rnd",
-	// 		"Mag_ASC_Stanag_20Rnd_Camo",
-	// 		"Mag_ASC_Stanag_20Rnd_DigiTan",
-	// 		"Mag_ASC_Stanag_20Rnd_Tan",
-	// 		"Mag_ASC_Stanag_20Rnd_UCP",
-	// 		"Mag_ASC_Stanag_100Rnd",
-	// 		"Mag_ASC_Stanag_100Rnd_Camo",
-	// 		"Mag_ASC_Stanag_100Rnd_DigiTan",
-	// 		"Mag_ASC_Stanag_100Rnd_Tan",
-	// 		"Mag_ASC_Stanag_100Rnd_UCP",
-	// 		"Mag_ASC_PMag_60Rnd",
-	// 		"Mag_ASC_PMag_60Rnd_Camo",
-	// 		"Mag_ASC_PMag_60Rnd_DigiTan",
-	// 		"Mag_ASC_PMag_60Rnd_Tan",
-	// 		"Mag_ASC_PMag_60Rnd_UCP",
-	// 		"BO_Mag_PMAG_30Rnd",
-	// 		"BO_Mag_PMAG_Tan_30Rnd",
-	// 		"BO_Mag_PMAG_Green_30Rnd",
-	// 		"BO_Mag_PMAGWindow_30Rnd", 
-	// 		"BO_Mag_D60_60Rnd",
-	// 		"BO_Mag_GEN2_100Rnd"
-	// 	};
-	// };
-	// class ASC_HK416_Base: Rifle_Base
-	// {
-	// 	magazines[]=
-	// 	{
-	// 		"Mag_STANAG_30Rnd",
-	// 		"Mag_STANAGCoupled_30Rnd",
-	// 		"Mag_STANAG_60Rnd",
-	// 		"Mag_CMAG_10Rnd",
-	// 		"Mag_CMAG_20Rnd",
-	// 		"Mag_CMAG_30Rnd",
-	// 		"Mag_CMAG_40Rnd",
-	// 		"Mag_CMAG_10Rnd_Green",
-	// 		"Mag_CMAG_20Rnd_Green",
-	// 		"Mag_CMAG_30Rnd_Green",
-	// 		"Mag_CMAG_40Rnd_Green",
-	// 		"Mag_CMAG_10Rnd_Black",
-	// 		"Mag_CMAG_20Rnd_Black",
-	// 		"Mag_CMAG_30Rnd_Black",
-	// 		"Mag_CMAG_40Rnd_Black",
-	// 		"Mag_ASC_Stanag_10Rnd",
-	// 		"Mag_ASC_Stanag_10Rnd_Camo",
-	// 		"Mag_ASC_Stanag_10Rnd_DigiTan",
-	// 		"Mag_ASC_Stanag_10Rnd_Tan",
-	// 		"Mag_ASC_Stanag_10Rnd_UCP",
-	// 		"Mag_ASC_Stanag_20Rnd",
-	// 		"Mag_ASC_Stanag_20Rnd_Camo",
-	// 		"Mag_ASC_Stanag_20Rnd_DigiTan",
-	// 		"Mag_ASC_Stanag_20Rnd_Tan",
-	// 		"Mag_ASC_Stanag_20Rnd_UCP",
-	// 		"Mag_ASC_Stanag_100Rnd",
-	// 		"Mag_ASC_Stanag_100Rnd_Camo",
-	// 		"Mag_ASC_Stanag_100Rnd_DigiTan",
-	// 		"Mag_ASC_Stanag_100Rnd_Tan",
-	// 		"Mag_ASC_Stanag_100Rnd_UCP",
-	// 		"Mag_ASC_PMag_60Rnd",
-	// 		"Mag_ASC_PMag_60Rnd_Camo",
-	// 		"Mag_ASC_PMag_60Rnd_DigiTan",
-	// 		"Mag_ASC_PMag_60Rnd_Tan",
-	// 		"Mag_ASC_PMag_60Rnd_UCP",
-	// 		"BO_Mag_PMAG_30Rnd",
-	// 		"BO_Mag_PMAG_Tan_30Rnd",
-	// 		"BO_Mag_PMAG_Green_30Rnd",
-	// 		"BO_Mag_PMAGWindow_30Rnd", 
-	// 		"BO_Mag_D60_60Rnd",
-	// 		"BO_Mag_GEN2_100Rnd"
-	// 	};
-	// };
+	class ASC_LVOAC_Base: Rifle_Base
+	{
+		magazines[]=
+		{
+			"Mag_STANAG_30Rnd",
+			"Mag_STANAGCoupled_30Rnd",
+			"Mag_STANAG_60Rnd",
+			"Mag_CMAG_10Rnd",
+			"Mag_CMAG_20Rnd",
+			"Mag_CMAG_30Rnd",
+			"Mag_CMAG_40Rnd",
+			"Mag_CMAG_10Rnd_Green",
+			"Mag_CMAG_20Rnd_Green",
+			"Mag_CMAG_30Rnd_Green",
+			"Mag_CMAG_40Rnd_Green",
+			"Mag_CMAG_10Rnd_Black",
+			"Mag_CMAG_20Rnd_Black",
+			"Mag_CMAG_30Rnd_Black",
+			"Mag_CMAG_40Rnd_Black",
+			"Mag_ASC_Stanag_10Rnd",
+			"Mag_ASC_Stanag_10Rnd_Camo",
+			"Mag_ASC_Stanag_10Rnd_DigiTan",
+			"Mag_ASC_Stanag_10Rnd_Tan",
+			"Mag_ASC_Stanag_10Rnd_UCP",
+			"Mag_ASC_Stanag_20Rnd",
+			"Mag_ASC_Stanag_20Rnd_Camo",
+			"Mag_ASC_Stanag_20Rnd_DigiTan",
+			"Mag_ASC_Stanag_20Rnd_Tan",
+			"Mag_ASC_Stanag_20Rnd_UCP",
+			"Mag_ASC_Stanag_100Rnd",
+			"Mag_ASC_Stanag_100Rnd_Camo",
+			"Mag_ASC_Stanag_100Rnd_DigiTan",
+			"Mag_ASC_Stanag_100Rnd_Tan",
+			"Mag_ASC_Stanag_100Rnd_UCP",
+			"Mag_ASC_PMag_60Rnd",
+			"Mag_ASC_PMag_60Rnd_Camo",
+			"Mag_ASC_PMag_60Rnd_DigiTan",
+			"Mag_ASC_PMag_60Rnd_Tan",
+			"Mag_ASC_PMag_60Rnd_UCP",
+			"BO_Mag_PMAG_30Rnd",
+			"BO_Mag_PMAG_Tan_30Rnd",
+			"BO_Mag_PMAG_Green_30Rnd",
+			"BO_Mag_PMAGWindow_30Rnd", 
+			"BO_Mag_D60_60Rnd",
+			"BO_Mag_GEN2_100Rnd"
+		};
+	};
+	class ASC_HK416_Base: Rifle_Base
+	{
+		magazines[]=
+		{
+			"Mag_STANAG_30Rnd",
+			"Mag_STANAGCoupled_30Rnd",
+			"Mag_STANAG_60Rnd",
+			"Mag_CMAG_10Rnd",
+			"Mag_CMAG_20Rnd",
+			"Mag_CMAG_30Rnd",
+			"Mag_CMAG_40Rnd",
+			"Mag_CMAG_10Rnd_Green",
+			"Mag_CMAG_20Rnd_Green",
+			"Mag_CMAG_30Rnd_Green",
+			"Mag_CMAG_40Rnd_Green",
+			"Mag_CMAG_10Rnd_Black",
+			"Mag_CMAG_20Rnd_Black",
+			"Mag_CMAG_30Rnd_Black",
+			"Mag_CMAG_40Rnd_Black",
+			"Mag_ASC_Stanag_10Rnd",
+			"Mag_ASC_Stanag_10Rnd_Camo",
+			"Mag_ASC_Stanag_10Rnd_DigiTan",
+			"Mag_ASC_Stanag_10Rnd_Tan",
+			"Mag_ASC_Stanag_10Rnd_UCP",
+			"Mag_ASC_Stanag_20Rnd",
+			"Mag_ASC_Stanag_20Rnd_Camo",
+			"Mag_ASC_Stanag_20Rnd_DigiTan",
+			"Mag_ASC_Stanag_20Rnd_Tan",
+			"Mag_ASC_Stanag_20Rnd_UCP",
+			"Mag_ASC_Stanag_100Rnd",
+			"Mag_ASC_Stanag_100Rnd_Camo",
+			"Mag_ASC_Stanag_100Rnd_DigiTan",
+			"Mag_ASC_Stanag_100Rnd_Tan",
+			"Mag_ASC_Stanag_100Rnd_UCP",
+			"Mag_ASC_PMag_60Rnd",
+			"Mag_ASC_PMag_60Rnd_Camo",
+			"Mag_ASC_PMag_60Rnd_DigiTan",
+			"Mag_ASC_PMag_60Rnd_Tan",
+			"Mag_ASC_PMag_60Rnd_UCP",
+			"BO_Mag_PMAG_30Rnd",
+			"BO_Mag_PMAG_Tan_30Rnd",
+			"BO_Mag_PMAG_Green_30Rnd",
+			"BO_Mag_PMAGWindow_30Rnd", 
+			"BO_Mag_D60_60Rnd",
+			"BO_Mag_GEN2_100Rnd"
+		};
+	};
 
-	// class BoltActionRifle_ExternalMagazine_Base;
-	// class BO_L96_Base: BoltActionRifle_ExternalMagazine_Base
-	// {
-	// 		weight=6800;
-	// };
+	class Ruger1022_Base;
+	class TWM_M14_Base: Ruger1022_Base
+	{
+		magazines[]=
+		{
+			"Mag_FAL_20Rnd",
+			"BO_Mag_M14_10rnd",
+			"BO_Mag_M14_20rnd",
+			"TWM_Mag_308Drum"
+		};
+	}
+
+	class TWM_IA2H_Base: FAL_Base
+	{
+		magazines[]=
+		{
+			"Mag_FAL_20Rnd",
+			"BO_Mag_M14_10rnd",
+			"BO_Mag_M14_20rnd",
+			"BO_Mag_FNFAL_30rnd",
+			"BO_Mag_FNFAL_40rnd",
+			"TWM_Mag_308Drum"
+		};
+	}
+
+	class TWM_M1A_Base: FAL_Base
+	{
+		magazines[]=
+		{
+			"Mag_FAL_20Rnd",
+			"BO_Mag_M14_10rnd",
+			"BO_Mag_M14_20rnd",
+			"BO_Mag_FNFAL_30rnd",
+			"BO_Mag_FNFAL_40rnd",
+			"TWM_Mag_308Drum"
+		};
+	}
+
+	class BoltActionRifle_ExternalMagazine_Base;
+	class BO_L96_Base: BoltActionRifle_ExternalMagazine_Base
+	{
+			weight=6800;
+	};
 };
